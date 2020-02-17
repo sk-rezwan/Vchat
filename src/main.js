7b2fdd2da9863a25b365dfe0a397ee225149d243
sk-rezwan <skrezwan12@gmail.com>
2020-02-17 17:02:01 +0600

    real time input 1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+// Required for side-effects
+require("firebase/firestore");
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyDzOZxV9A8NDQwyI9y7y6GAKdbf9Y-n8rU",
+  authDomain: "vchat-2dd8d.firebaseapp.com",
+  databaseURL: "https://vchat-2dd8d.firebaseio.com",
+  projectId: "vchat-2dd8d",
+  storageBucket: "vchat-2dd8d.appspot.com",
+  messagingSenderId: "265792822742",
+  appId: "1:265792822742:web:7f0ff0349c706620a69877",
+  measurementId: "G-SBNW1WWRFX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.firestore();
+
+window.db = db; //can access from anywhere
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import PrivateChat from '../views/PrivateChat.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: PrivateChat
   },
   {
     path: '/about',
